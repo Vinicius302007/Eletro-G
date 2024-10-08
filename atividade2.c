33aad3aca5483199680ff6bb72f74804b4e21dfb
Vinicius302007 <vinisantiago2007@gmail.com>
2024-10-08 09:49:26 -0300

    Add files via upload

---
+++ atividade2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int idadeVinicius = 16;
+    int idadeEduardo = 17;
+
+    if(idadeVinicius > idadeEduardo){
+    printf("Vinicius e mais novo que Eduardo!");
+}
+    else{
+        printf ("Eduardo e menos novo que Vinicius");}
+}

404870a0f979d62a09f8959ca7da666bcef4c2df
Vinicius302007 <vinisantiago2007@gmail.com>
2024-10-08 11:14:15 -0300

    Add files via upload

---
+++ atividade4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+float n1;
+float n2;
+float n3;
+float n4;
+printf (" digite n1:");
+scanf("%f" , &n1);
+printf("digite n2:");
+scanf("%f" , &n2);
+printf("digite n3:");
+scanf("%f" , &n3);
+printf("digite n4:");
+scanf("%f" , &n4);
+float media;
+media = (n1 + n2 + n3 + n4)/4;
+
+if (media > 4 && media < 7){
+    printf("o aluno esta de exame final");
+}
+
+if(media . 7){
+    printf("aluno esta aprovado");
+}
+if(media < 4){
+    printf("aluno esta reprovado");
+}
+}
+

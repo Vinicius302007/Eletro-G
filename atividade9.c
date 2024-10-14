f6c182a924f9b10840fbf460bd2d4dcadc18cc84
Vinicius302007 <vinisantiago2007@gmail.com>
2024-10-14 12:58:59 -0300

    Add files via upload

---
+++ atividade9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, temp;
+
+
+    printf("Digite três números distintos:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+ 
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a > c) {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b > c) {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("Números em ordem crescente: %d, %d, %d\n", a, b, c);
+}
